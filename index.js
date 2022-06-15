532729d7dc63a7fd6ef4c5e7eaa60e45db6f6901
bappi511 <bappyhossain018@gmail.com>
2022-06-15 14:22:21 +0600

    project init

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const cors = require('cors');
+const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qbuxbv9.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        await client.connect();
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
+
+//middleware;
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('max tools server running')
+});
+
+app.listen(port, () => {
+    console.log('server running at', port);
+})
